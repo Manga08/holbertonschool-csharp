366ee5c3f43581ce5c922aa423bd4fdb8c978e50
Manga08 <bijuu5445@gmail.com>
2020-12-02 22:37:36 -0500

    [Featured]: The 8 Task

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int i = 0;
+        foreach (KeyValuePair<string, string> keys in myDict)
+            i++;
+        return i;
+    }
+}
