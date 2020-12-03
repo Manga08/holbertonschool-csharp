a6bf5d0ecf25378ceffe5014d9ed663698ab9a65
Manga08 <bijuu5445@gmail.com>
2020-12-02 22:43:07 -0500

    [Featured]: The 9 Task

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        if (!myDict.ContainsKey(key)){
+            myDict.Add(key, value);
+            return myDict;
+        }
+        else {
+            myDict[key] = value;
+            return myDict;
+        }
+    }
+}
